195475652a22c3fa64f7528200038b3d339243e1
j1xaro <konecnik.jan@gmail.com>
2020-03-16 23:04:20 +0100

    1.

---
+++ doktor-frontend/src/components/loginpage.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoginOpen: true,
+        isRegisterOpen: false
+      };
+    }
+  
+    render() {
+  
+      return (
+        <div className="root-container">
+   
+        </div>
+      );
+    }
+  }
+  reactDOM.render(
+    <App />, document.getElementById("root"));
